72fb0fbbbdeae4ff44dd4059f72a241b168ff856
HoloBlink <47296083+HoloBlink@users.noreply.github.com>
2019-05-17 18:15:17 +0200

    MASTER_INIT

---
+++ WESTERN_IA/Assets/Environment/Scripts/CowboyLocomotion.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CowboyLocomotion : MonoBehaviour
+{
+    public GameObject Target;
+    private NavMeshAgent _navAgent;
+
+    void Awake()
+    {
+        _navAgent = GetComponentInParent<NavMeshAgent>();
+
+    }
+
+    void Update()
+    {
+        if (Target != null)
+        {
+            if (!_navAgent.hasPath)
+            {
+                MoveTo(Target.transform);
+            }
+        }
+    }
+
+    public void MoveTo(Transform target)
+    {
+        _navAgent.destination = target.position;
+        _navAgent.gameObject.transform.LookAt(_navAgent.destination);
+        
+    }
+}
---
+++ WESTERN_IA/Assets/Environment/Scripts/PCLocomotion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCLocomotion : MonoBehaviour
+{
+    public GameObject RootElement;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    var x = Input.GetAxis("Horizontal") * Time.deltaTime * 10.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 10.0f;
+	    var y = Input.GetAxis("Rotate") * Time.deltaTime * 180.0f;
+        RootElement.transform.Rotate(0, y, 0);
+        RootElement.transform.Translate(x, 0, z);
+        
+    }
+}
